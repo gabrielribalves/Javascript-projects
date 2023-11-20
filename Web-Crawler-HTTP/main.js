c4e3bb9bbb2aa17695515b00982aadd8aaf41079
Gabriel Ribeiro <gabriel.ribalves@gmail.com>
2023-11-20 11:14:24 -0300

    crawl page recursive

--- Web-Crawler-HTTP/main.js
+++ Web-Crawler-HTTP/main.js
@@ -1,6 +1,6 @@
 const {crawlPage} = require('./crawl.js')
 
-function main(){
+async function main(){
     if(process.argv.length < 3){
         console.log("no website");
         process.exit(1)
@@ -10,7 +10,12 @@ function main(){
     }
     const baseURL = process.argv[2]
     console.log(`starting crawl ${baseURL}`);
-    crawlPage(baseURL)
+    const pages = await crawlPage(baseURL, baseURL, {})
+    // console.log(pages);
+
+    for(const page of Object.entries(pages)){
+        console.log(page);
+    }
 }
 
 main()
